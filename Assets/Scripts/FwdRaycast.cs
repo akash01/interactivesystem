97e8e70d875492b03fa2a7a1c8160cb82826449e
Chris Blythe <christian.blythe@cs.helsinki.fi>
2014-03-28 15:58:58 +0200

    Ray casting implemented

---
+++ Assets/Scripts/FwdRaycast.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FwdRaycast : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var fwd = transform.TransformDirection(Vector3.forward);
+		Color color = Color.green;
+		RaycastHit hit;
+
+		if (Physics.Raycast (transform.position, fwd, out hit, 10)){
+
+			if (hit.collider != null) {
+				color = Color.red;
+				hit.collider.SendMessage ("OnRayDetect", SendMessageOptions.DontRequireReceiver);
+			}
+		}
+		Debug.DrawRay(transform.position, fwd * 5, color);
+	}
+}
---
+++ Assets/Scripts/ObjectReactor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectReactor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnRayDetect() {
+		print("Upps, detected");
+	}
+}
