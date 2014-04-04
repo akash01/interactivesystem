6b778c819698d11bc9826b2c63d851d8b878a654
mukesh <pace.akash@gmail.com>
2014-04-04 16:56:54 +0300

    avatar animation working

--- Assets/Scripts/ObjectReactor.cs
+++ Assets/Scripts/ObjectReactor.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class ObjectReactor : MonoBehaviour {
 
+	int count = 0;
+	bool lastHit = false;
+	bool thisHit = false;
+	bool hit = false;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,10 +15,43 @@ public class ObjectReactor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
+		if ( thisHit && !lastHit ) 
+		{
+			hit = true;
+			//animation.Play("SmileIn");
+		}
+
+		if ( !thisHit && lastHit ) 
+		{
+			hit = false;
+			//animation.Play("SmileIn");
+		}
+
+		print(hit);
+
+		lastHit = thisHit;
+		thisHit = false;
+
+
+		//print (lastHit );
 	}
 
 	public void OnRayDetect() {
-		print("Upps, detected");
+		//print("Upps, detected " + count);
+		thisHit = true;
+		count++;
+		//animation.Play("SmileIn");
+		StartCoroutine(Pause());
+		//animation.Play("SmileOut");
+
+	}
+
+	IEnumerator Pause() {
+		//animation.Play("SmileIn");
+		yield return new WaitForSeconds(1);
+		//animation.Play("SmileOut");
+
 	}
 }
