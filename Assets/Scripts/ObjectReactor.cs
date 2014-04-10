a2c0b95cdc6983ca8588a7e6020bf3f0ab04eb23
Chris Blythe <christian.blythe@cs.helsinki.fi>
2014-04-11 00:29:04 +0300

    Code for facial animation working
    Animation starts on first raytrace hit of avatar
    Adjustable pause variable before expression in plays
    Adjustable pause variable between expression in and out
    No more expressions possible until current finished

--- Assets/Scripts/ObjectReactor.cs
+++ Assets/Scripts/ObjectReactor.cs
@@ -4,9 +4,16 @@ using System.Collections;
 public class ObjectReactor : MonoBehaviour {
 
 	int count = 0;
-	bool lastHit = false;
-	bool thisHit = false;
+	bool thisRayDetect = false;
+	bool lastRayDetect = false;
 	bool hit = false;
+	bool lastHit = false;
+	bool animationReady = true;
+
+	//Temp global variables
+	string expression = "smile";
+	int initialPause = 1;
+	int expressionPause = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -17,38 +24,80 @@ public class ObjectReactor : MonoBehaviour {
 	void Update () {
 
 		// Set up 'hit' variable - true if raytrace hitting avatar box collider, false if not
-		if ( thisHit && !lastHit ) 
+		if ( thisRayDetect && !lastRayDetect ) 
 		{
 			hit = true;
 		}
 
-		if ( !thisHit && lastHit ) 
+		if ( !thisRayDetect && lastRayDetect ) 
 		{
 			hit = false;
 		}
-							//TEST PRINT LINE - REMOVE
-							print(hit);
-		lastHit = thisHit;
-		thisHit = false;
-		
+
+		lastRayDetect = thisRayDetect;
+		thisRayDetect = false;
+
+		//TEST PRINT LINE - REMOVE
+		print(hit);
+
 		// Trigger expression animation on first hit (when raytrace first hits collider)
+		if ( hit && !lastHit ) 
+		{
+			if (animationReady) {
+				//Stop an more animations playing until current on finished
+				animationReady = false;
+				//Play expression animation
+				StartCoroutine(playExpression());
+			}
+
+		}
+		
+		if ( !thisRayDetect && lastRayDetect ) 
+		{
+			hit = false;
+		}
+
 
 	}
 
 	public void OnRayDetect() {
-		//print("Upps, detected " + count);
-		thisHit = true;
-		count++;
-		//animation.Play("SmileIn");
-		StartCoroutine(Pause());
-		//animation.Play("SmileOut");
-
+		//Record of raytrace hit on this frame
+		thisRayDetect = true;
 	}
 
-	IEnumerator Pause() {
-		//animation.Play("SmileIn");
+	//Play expression based on global variables
+	IEnumerator playExpression() {
+		string animationIn = "SmileIn";
+		string animationOut = "SmileOut";
+
+		//Set expression animations based on 'expression' global
+		if (expression == "smile") {
+			animationIn = "SmileIn";
+			animationOut = "SmileOut";
+		}
+		if (expression == "frown") {
+			animationIn = "frownIn";
+			animationOut = "frownOut";
+		}
+
+		//Wait for initial pause (from global)
+		yield return new WaitForSeconds(initialPause);
+		//Play in animation
+		animation.Play(animationIn);
+		//Wait for pause in middle of expression (from global) 
+		//Plus second to allow in animation to finish
+		yield return new WaitForSeconds(expressionPause + 1);
+		//Play out animation
+		animation.Play(animationOut);
+		//Wait for out animation to finish
 		yield return new WaitForSeconds(1);
-		//animation.Play("SmileOut");
-
+		//Allow a new animation to start
+		animationReady = true;
 	}
+
+
+
+
+
+
 }
